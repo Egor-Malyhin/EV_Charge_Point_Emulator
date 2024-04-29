ed03f5684d26541f61e2c3a977250aa428f31fef
Egor <evmalyhin@mail.ru>
2024-04-29 17:16:04 +0700

    Minor Code Changes

---
+++ src/main/java/org/mycorp/stateoperator/StateOperator.java
@@ -0,0 +1,11 @@
+package org.mycorp.stateoperator;
+
+import org.mycorp.models.StationState;
+
+public class StateOperator implements StateOperatorInterface {
+
+    @Override
+    public void setStationState(StationState stationState) {
+
+    }
+}
---
+++ src/main/java/org/mycorp/stateoperator/StateOperatorInterface.java
@@ -0,0 +1,7 @@
+package org.mycorp.stateoperator;
+
+import org.mycorp.models.StationState;
+
+public interface StateOperatorInterface {
+    void setStationState(StationState stationState);
+}
